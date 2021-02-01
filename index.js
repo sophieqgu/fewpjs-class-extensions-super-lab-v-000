1391e92b616b6111d6617501b021daf0b7469df6
Sophie Gu <sophieqgu@gmail.com>
2021-02-01 03:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ class Deciduous extends Tree {
   }
 
   static definition() {
-    return `${super.definition}`
+    return `${super.definition} Deciduous trees shed their leaves annually.`
+  }
+}
+
+
+class Evergreen extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+
+  static definition() {
+    return `${super.definition} Deciduous trees shed their leaves annually.`
   }
 }
