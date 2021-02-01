420234bd1cdc74c4a7ceb7e7c04a42349afbaaab
Sophie Gu <sophieqgu@gmail.com>
2021-02-01 03:27:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ class Evergreen extends Tree {
   }
 
   static definition() {
-    return `${super.definition} Deciduous trees shed their leaves annually.`
+    return `${super.definition} Evergreens keep their leaves all year round.`
   }
 }
